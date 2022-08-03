9128b746b75850a3c9657b67ed357949ec8bb086
Andrew Michael Toivonen <andrew.toivonen@ldas-pcdev6.ligo.caltech.edu>
2022-08-03 09:43:30 -0700

    Resolving comments

--- ligo/em_bright/lightcurves/lightcurve_utils.py
+++ ligo/em_bright/lightcurves/lightcurve_utils.py
@@ -5,18 +5,48 @@ from scipy.stats import rv_continuous
 from scipy.integrate import quad
 
 
-def load_EOS_posterior():
-    # load EOS posterior draws (https://zenodo.org/record/6502467#.Yoa2EKjMI2z)
+def load_eos_posterior():
+    '''
+    loads eos posterior draws (https://zenodo.org/record/6502467#.Yoa2EKjMI2z)
+
+    Returns
+    -------
+    draws: np.array
+        equally weighted eos draws from file
+    '''
     rel_path = 'data/LCEHL_EOS_posterior_samples_PSR+GW_slice.h5'
-    EOS_path = Path(__file__).parents[1] / rel_path
-    with h5py.File(EOS_path, 'r') as f:
-        # dset = h5py.File(f, 'r')
+    eos_path = Path(__file__).parents[1] / rel_path
+    with h5py.File(eos_path, 'r') as f:
         draws = np.array(f['EOS'])
     return draws
 
 
+def make_distribution(foo):
+    class _MakeDistribution(rv_continuous):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.normalize, _ = quad(foo, self.a, self.b)
+
+        def _pdf(self, m):
+            return foo(m) / self.normalize
+    return _MakeDistribution
+
+
+@make_distribution
 def alsing_pdf(m):
-    # From Alsing 2018 (arxiv 1709.07889)
+    '''
+    From Alsing 2018 (arxiv 1709.07889)
+
+    Parameters
+    ----------
+    m: float or np.array
+        mass(es) at which the PDF is evaluated
+
+    Returns
+    -------
+    PDF: float or np.array
+        PDF value at m
+    '''
     mu1, s1, mu2, s2, a = 1.34, 0.07, 1.8, 0.21, 2.12
     PDF1 = a/(s1*np.sqrt(2*np.pi))*np.exp(-((m-mu1)/(np.sqrt(2)*s1))**2)
     PDF2 = a/(s2*np.sqrt(2*np.pi))*np.exp(-((m-mu2)/(np.sqrt(2)*s2))**2)
@@ -24,8 +54,22 @@ def alsing_pdf(m):
     return PDF
 
 
+@make_distribution
 def farrow_pdf(m):
-    # From Farrow 2019 (arxiv 1902.03300)
+    '''
+    From Farrow 2019 (arxiv 1902.03300)
+
+
+    Parameters
+    ----------
+    m: float or np.array
+        mass(es) at which the PDF is evaluated
+
+    Returns
+    -------
+    PDF: float or np.array
+        PDF value at m
+    '''
     mu1, s1, mu2, s2, a = 1.34, 0.02, 1.47, 0.15, 0.68
     PDF1 = a/(s1*np.sqrt(2*np.pi))*np.exp(-((m-mu1)/(np.sqrt(2)*s1))**2)
     PDF2 = (1-a)/(s2*np.sqrt(2*np.pi))*np.exp(-((m-mu2)/(np.sqrt(2)*s2))**2)
@@ -33,38 +77,21 @@ def farrow_pdf(m):
     return PDF
 
 
+@make_distribution
 def zhu_pdf(m):
-    # From Zhu 2021 (arxiv 2011.02717)
+    '''
+    From Zhu 2021 (arxiv 2011.02717)
+
+    Parameters
+    ----------
+    m: float or np.array
+        mass(es) at which the PDF is evaluated
+
+    Returns
+    -------
+    PDF: float or np.array
+        PDF value at m
+    '''
     a1, a2, a3, b1, b2, b3 = .002845, 1.04e11, 799.1, 1.686, 2.1489, .2904
     PDF = 1/(1/(a1*np.exp(b1*m))+1/(a2*np.exp(-b2*m)+a3*np.exp(-b3*m)))
     return PDF
-
-
-class alsing_dist(rv_continuous):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.normalize, _ = quad(alsing_pdf, self.a, self.b)
-
-    def _pdf(self, m):
-        return alsing_pdf(m) / self.normalize
-
-
-class farrow_dist(rv_continuous):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.normalize, _ = quad(farrow_pdf, self.a, self.b)
-
-    def _pdf(self, m):
-        return farrow_pdf(m) / self.normalize
-
-
-class zhu_dist(rv_continuous):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.normalize, _ = quad(zhu_pdf, self.a, self.b)
-
-    def _pdf(self, m):
-        return zhu_pdf(m) / self.normalize
--- ligo/em_bright/tests/test_gwlc.py
+++ ligo/em_bright/tests/test_gwlc.py
@@ -4,7 +4,9 @@ from unittest.mock import patch
 from gwemlightcurves.KNModels import KNTable
 from ..lightcurves import calc_lightcurves
 from ..lightcurves.mass_distributions import BNS_alsing, BNS_farrow, NSBH_zhu
-from ..lightcurves.lightcurve_utils import load_EOS_posterior
+from ..lightcurves.mass_distributions import BNS_uniform, BNS_LRR
+from ..lightcurves.mass_distributions import NSBH_uniform, NSBH_LRR
+from ..lightcurves.lightcurve_utils import load_eos_posterior
 
 
 # m1, m2 values for Alsing, Farrow, Zhu initial NS/BH mass dists
@@ -18,12 +20,28 @@ far_result = [[1.75185058, 2.44593256, 1.99912136],
 zhu_result = [[8.24086231, 9.91239175, 6.84081889],
               [1.33484312, 1.3357914, 1.32818417]]
 
+BNS_uni_result = [[2.4268996, 2.70593716, 2.18786574],
+                  [1.34370059, 1.30106652, 1.98714215]]
+
+BNS_LRR_result = [[1.45691842, 1.46629522, 1.30002483],
+                  [1.30820654, 1.1892853, 1.27899721]]
+
+NSBH_uni_result = [[6.08817749, 6.06047861, 6.08466998],
+                   [2.70437173, 2.25909451, 1.71116343]]
+
+NSBH_LRR_result = [[1.55573485, 2.50949848, 1.43821419],
+                   [1.33426283, 1.44072616, 1.04894091]]
+
 
 @pytest.mark.parametrize(
     'dist, result',
     [[BNS_alsing, als_result],
      [BNS_farrow, far_result],
      [NSBH_zhu, zhu_result],
+     [BNS_uniform, BNS_uni_result],
+     [BNS_LRR, BNS_LRR_result],
+     [NSBH_uniform, NSBH_uni_result],
+     [NSBH_LRR, NSBH_LRR_result],
      ]
 )
 def test_initial_mass_draws(dist, result):
@@ -41,15 +59,14 @@ def test_initial_mass_draws(dist, result):
         assert(np.abs(m - r) < 1e-6)
 
 
-dyn_result = [0.01858454429603665, 0.020577048962047758,
-              0.024680568573388153, 0.024708485066836983,
-              0.01865444152290588, 0.023558982026319016,
-              0.0, 0.01312538012541829, 0.0, 0.0]
+wind_result = [0.0256483901004048, 0.00015, 0.00015,
+               0.0, 0.0, 0.0, 0.0, 0.00015,
+               0.024255867289002035, 0.00015]
 
-wind_result = [0.00015, 0.00015, 0.018125212769565864,
-               0.019958112321618027, 0.00015,
-               0.026042143314813155, 0.001797551492723432,
-               0.00015, 0.0, 0.0]
+dyn_result = [0.026983497953596173, 0.02016999499804752,
+              0.01770449656270658, 0.0, 0.0, 0.0, 0.0,
+              0.023883542614886615, 0.02623057446187879,
+              0.013457388566138451]
 
 
 @pytest.mark.parametrize(
@@ -57,12 +74,12 @@ wind_result = [0.00015, 0.00015, 0.018125212769565864,
     [[np.array([2.1]), np.array([1.5]), np.array([45]),
       wind_result, dyn_result]]
 )
-def test_run_EOS(m1, m2, thetas, wind_result, dyn_result):
-    draws = load_EOS_posterior()
-    # number of EOS draws, in this case the number of EOS files
+def test_run_eos(m1, m2, thetas, wind_result, dyn_result):
+    draws = load_eos_posterior()
+    # number of eos draws, in this case the number of eos files
     N_draws = 10
-    samples = calc_lightcurves.run_EOS(m1, m2, thetas,
-                                       N_EOS=N_draws, EOS_draws=draws)
+    samples, _ = calc_lightcurves.run_eos(m1, m2, thetas, N_eos=N_draws,
+                                          eos_draws=draws)
     wind_mej, dyn_mej = samples['wind_mej'], samples['dyn_mej']
     # check wind and dyn mej values exist
     assert(len(wind_mej) > 0)
@@ -73,14 +90,14 @@ def test_run_EOS(m1, m2, thetas, wind_result, dyn_result):
     for m, r in zip(dyn_mej, dyn_result):
         assert(np.abs(m - r) < 1e-6)
 
-    # check that merger type matches NS/BH definition from EOS
+    # check that merger type matches NS/BH definition from eos
     for sample in samples:
-        if sample['merger_type'] == 1:
-            assert (samples['m1'] <= samples['mbns']) & (samples['m2'] <= samples['mbns'])  # noqa:E501
-        elif sample['merger_type'] == 2:
-            assert (samples['m1'] > samples['mbns']) & (samples['m2'] <= samples['mbns'])  # noqa:E501
-        elif sample['merger_type'] == 3:
-            assert (samples['m1'] > samples['mbns']) & (samples['m2'] > samples['mbns'])  # noqa:E501
+        if sample['merger_type'] == 'BNS':
+            assert (sample['m1'] <= sample['mbns']) & (sample['m2'] <= sample['mbns'])  # noqa:E501
+        elif sample['merger_type'] == 'NSBH':
+            assert (sample['m1'] > sample['mbns']) & (sample['m2'] <= sample['mbns'])  # noqa:E501
+        elif sample['merger_type'] == 'BBH':
+            assert (sample['m1'] > sample['mbns']) & (sample['m2'] > sample['mbns'])  # noqa:E501
 
 
 @pytest.mark.parametrize(
@@ -88,13 +105,14 @@ def test_run_EOS(m1, m2, thetas, wind_result, dyn_result):
     [[np.array([10.0]), np.array([1.5]), np.array([45])]]
 )
 def test_high_mass_ratio(m1, m2, thetas):
-    draws = load_EOS_posterior()
-    # number of EOS draws, in this case the number of EOS files
+    # a high mass ratio merger should produce very little mass ejecta
+    draws = load_eos_posterior()
+    # number of eos draws, in this case the number of eos files
     N_draws = 10
-    samples = calc_lightcurves.run_EOS(m1, m2, thetas,
-                                       N_EOS=N_draws, EOS_draws=draws)
+    samples, _ = calc_lightcurves.run_eos(m1, m2, thetas, N_eos=N_draws,
+                                          eos_draws=draws)
     mej = samples['mej']
-    # check high mass ratio has low mej
+    # check high mass ratio has low mej on average
     assert (np.mean(mej) < 1e-3)
 
 
@@ -103,13 +121,14 @@ def test_high_mass_ratio(m1, m2, thetas):
     [[np.array([1.4]), np.array([1.4]), np.array([45])]]
 )
 def test_low_mass_wind_check(m1, m2, thetas):
-    draws = load_EOS_posterior()
-    # number of EOS draws, in this case the number of EOS files
+    # a low mass BNS merger should produce significant wind ejecta
+    draws = load_eos_posterior()
+    # number of eos draws, in this case the number of eos files
     N_draws = 10
-    samples = calc_lightcurves.run_EOS(m1, m2, thetas,
-                                       N_EOS=N_draws, EOS_draws=draws)
+    samples, _ = calc_lightcurves.run_eos(m1, m2, thetas, N_eos=N_draws,
+                                          eos_draws=draws)
     wind_mej = samples['wind_mej']
-    # low mass BNS should have significant wind mej
+    # check for significant wind mej on average
     assert (np.mean(wind_mej) > 1e-3)
 
 
@@ -118,13 +137,14 @@ def test_low_mass_wind_check(m1, m2, thetas):
     [[np.array([2.4]), np.array([2.4]), np.array([45])]]
 )
 def test_high_mass_wind_check(m1, m2, thetas):
-    draws = load_EOS_posterior()
-    # number of EOS draws, in this case the number of EOS files
+    # a high mass BNS merger should produce little wind ejecta
+    draws = load_eos_posterior()
+    # number of eos draws, in this case the number of eos files
     N_draws = 10
-    samples = calc_lightcurves.run_EOS(m1, m2, thetas,
-                                       N_EOS=N_draws, EOS_draws=draws)
+    samples, _ = calc_lightcurves.run_eos(m1, m2, thetas, N_eos=N_draws,
+                                          eos_draws=draws)
     wind_mej = samples['wind_mej']
-    # high mass BNS should have small wind mej
+    # check for minimal wind ejecta
     assert (np.mean(wind_mej) < 1e-3)
 
 
@@ -132,13 +152,13 @@ def test_high_mass_wind_check(m1, m2, thetas):
     'samples',
     [KNTable(([.1], [35], [0]), names=('mej', 'theta', 'sample_id'))]
 )
-def test_ejecta_to_lc(samples):
+def test_ejecta_to_lightcurve(samples):
     mags = [np.ones((9, 500))]
     t = [np.ones(500)]
     mock_mags = KNTable((t, mags), names=('t', 'mag'))
     with patch('gwemlightcurves.KNModels.KNTable.model') as mock_KNTable:
         mock_KNTable.return_value = mock_mags
-        lightcurve_data = calc_lightcurves.ejecta_to_lc(samples)
+        lightcurve_data, _ = calc_lightcurves.ejecta_to_lightcurve(samples)
         # check the mock object was called once, and has the right value
         mock_KNTable.assert_called_once()
         assert mock_KNTable.return_value is mock_mags
@@ -152,7 +172,11 @@ def test_ejecta_to_lc(samples):
     [[np.array([1.5, 4.0]), np.array([1.5, 1.2]), np.array([45, 45])]]
 )
 def test_lightcurve_predictions(m1, m2, theta):
-    lightcurve_data1, has_Remnant1 = calc_lightcurves.lightcurve_predictions(mass_dist=BNS_alsing, N_EOS=4, mass_draws=1)  # noqa:E501
-    lightcurve_data2, has_Remnant2 = calc_lightcurves.lightcurve_predictions(m1s=m1, m2s=m2, thetas=theta, N_EOS=2)  # noqa:E501
+    # check that has_Remnant is working both for a mass
+    # distribution and when passing specific masses
+    # BNS alsing will usually produce significant ejecta
+    lightcurve_data1, has_Remnant1, _, _ = calc_lightcurves.lightcurve_predictions(mass_dist=BNS_alsing, N_eos=2, mass_draws=2)  # noqa:E501
+    # NSBH mergers will frequently produce minimal mass ejecta
+    lightcurve_data2, has_Remnant2, _, _ = calc_lightcurves.lightcurve_predictions(m1s=m1, m2s=m2, thetas=theta, N_eos=2)  # noqa:E501
     assert has_Remnant1 == 1.0
     assert has_Remnant2 == 0.5
