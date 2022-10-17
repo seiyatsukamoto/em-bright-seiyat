f07edf23827c1d1c4fa7a15d7e0d39e3215945c7
duncan.macleod <duncan.macleod@ligo.org>
2022-10-17 14:10:38 +0100

    use ThreadPoolExecutor to speed up data file d/l

--- ligo/em_bright/__init__.py
+++ ligo/em_bright/__init__.py
@@ -1,6 +1,16 @@
 import json
+from concurrent.futures import (
+    as_completed,
+    ThreadPoolExecutor,
+)
+from os import getenv
+from os.path import basename
 
-from astropy.utils.data import download_file
+from astropy.utils.console import ProgressBar
+from astropy.utils.data import (
+    download_file,
+    is_url_in_cache,
+)
 
 __version__ = '1.0.3'
 
@@ -37,12 +47,57 @@ PACKAGE_DATA_LINKS = {name: f'{PACKAGE_DATA_BASE_URL}/{name}' for name in (
     'EOS_MAX_MASS_MAP.json',
 )}
 
-PACKAGE_FILENAMES = dict.fromkeys(PACKAGE_DATA_LINKS)
 
-for name, url in PACKAGE_DATA_LINKS.items():
-    PACKAGE_FILENAMES[name] = download_file(
-        url, cache=True, pkgname='ligo.em_bright'
+def _download_data_file(url):
+    """Download a single file.
+
+    This is an internal method designed to be called as part of a map()
+    operation only.
+    """
+    return download_file(
+        url,
+        cache=True,
+        show_progress=False,
+        pkgname="ligo.em_bright",
     )
+
+
+def _download_data_files(
+    urls,
+    message=None,
+    max_workers=int(getenv("OMP_NUM_THREADS", 0)) or None,
+    **kwargs,
+):
+    """Download a list of data files using a thread pool.
+    """
+    urls = list(urls)
+    # only download files we need (mainly to constrain the progress bar)
+    needed = list(filter(
+        lambda x: not is_url_in_cache(x, pkgname="ligo.em_bright"),
+        urls,
+    ))
+    if needed:
+        if message:
+            print(message)
+        # use a threadpool to speed things up
+        with ProgressBar(len(needed)) as bar, \
+             ThreadPoolExecutor(max_workers=max_workers, **kwargs) as executor:
+            for i, result in enumerate(as_completed(
+                executor.submit(_download_data_file, url)
+                for url in needed
+            )):
+                bar.update(i+1)
+    # run it again with _all_ of the files to get the local paths
+    return dict(zip(
+        map(basename, urls),
+        map(_download_data_file, urls)
+    ))
+
+
+PACKAGE_FILENAMES = _download_data_files(
+    PACKAGE_DATA_LINKS.values(),
+    message="Downloading ligo.em_bright data files...",
+)
 
 # load and normalize bayes factors
 with open(PACKAGE_FILENAMES['EOS_BAYES_FACTOR_MAP.json']) as f:
