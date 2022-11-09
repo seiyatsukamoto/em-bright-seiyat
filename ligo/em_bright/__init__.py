2a71a7d6d42c74d25a35aace70b969ae41f5923e
Sushant Sharma-Chaudhary <sushant.sharma-chaudhary@ldas-pcdev12.ligo.caltech.edu>
2022-11-09 14:58:27 -0800

    update classifiers using sklearn v1.1.3
    Downloading datafiles from main branch until next release

--- ligo/em_bright/__init__.py
+++ ligo/em_bright/__init__.py
@@ -13,11 +13,11 @@ from astropy.utils.data import (
 )
 
 __version__ = '1.0.4'
-
 PACKAGE_DATA_BASE_URL = (
     'https://git.ligo.org/emfollow/em-properties/em-bright/'
-    f'-/raw/v{__version__}/ligo/em_bright/data'
+    '-/raw/main/ligo/em_bright/data'
 )
+
 PACKAGE_DATA_LINKS = {name: f'{PACKAGE_DATA_BASE_URL}/{name}' for name in (
     'equil_2H.dat',
     'APR4_EPP.pickle',
